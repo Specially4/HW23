2e876a275865a4fecfeacc40ba7d1c1980f18d5e
Specially4 <kotenkovsn@gmail.com>
2022-08-19 21:51:29 +0700

    add annotations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+from typing import Dict, Tuple, Any, Optional, Union
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from werkzeug.exceptions import BadRequest
 
 from constants import DATA_DIR
@@ -10,16 +11,17 @@ app = Flask(__name__)
 
 
 @app.route("/perform_query", methods=["POST"])
-def perform_query():
-    data: dict = request.json
-    file_name: str = data['file_name']
+def perform_query() -> Tuple[Response, int]:
+    try:
+        data: Dict[str, str] = request.json
+        file_name = data['file_name']
+    except KeyError:
+        raise BadRequest
 
     if not os.path.exists(os.path.join(DATA_DIR, file_name)):
         raise BadRequest
 
-    return jsonify(query(data)), 200
-
-    # return app.response_class('', content_type="text/plain")
+    return app.response_class(query(data), content_type='text/plain'), 200
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 import os
+import re
+from typing import List, Dict
 
 from werkzeug.exceptions import BadRequest
 
 from constants import DATA_DIR
 
 
-def do_cmd(cmd: str, value: str | int, data) -> list:
+def do_cmd(cmd: str, value:  str, data: List[str]) -> List[str]:
     if cmd == 'filter':
-        result = list(filter(lambda record: value in record, data))
+        result: List[str] = list(filter(lambda record: value in record, data))
     elif cmd == 'map':
         col_num = int(value)
         result = list(map(lambda record: record.split()[col_num], data))
@@ -18,21 +20,24 @@ def do_cmd(cmd: str, value: str | int, data) -> list:
         result = sorted(data, reverse=reverse)
     elif cmd == 'limit':
         result = data[:int(value)]
+    elif cmd == 'regex':
+        regex = re.compile(str(value))
+        result = list(filter(lambda v: regex.search(v), data))
     else:
         raise BadRequest
 
     return result
 
 
-def query(params) -> list:
+def query(params: Dict[str, str]) -> list:
     with open(os.path.join(DATA_DIR, params['file_name'])) as f:
         file_data = f.readlines()
 
     res = file_data
 
     if 'cmd1' in params.keys():
-        res = do_cmd(params['cmd1'], params['value1'], res)
+        res = do_cmd(cmd=params['cmd1'], value=params['value1'], data=res)
     if 'cmd2' in params.keys():
-        res = do_cmd(params['cmd2'], params['value2'], res)
+        res = do_cmd(cmd=params['cmd2'], value=params['value2'], data=res)
 
     return res
